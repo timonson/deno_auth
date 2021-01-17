48343c3b7eefc8c10a2f0a2b874c7b043a627ffd
timonson <timo.rossa@tutanota.com>
2021-01-17 21:18:38 +0100

    Move loginAuto to directory authentication

--- rpcMethods.ts
+++ rpcMethods.ts
@@ -2,20 +2,8 @@ import {
   authenticate,
   confirmRegistration,
   login,
+  loginAuto,
   register,
 } from "./authentication/mod.ts";
-import { Payload } from "./crypto/mod.ts";
-import { foldResult, ifFailed, ServerRequest } from "./deps.ts";
-
-async function loginAuto({ req }: { req: ServerRequest }) {
-  return await foldResult(async (payload: Payload) => ({
-    isSuccess: true,
-    html: await Deno.readTextFile("./static/private/index.html"),
-    email: payload.email,
-    user: payload.user,
-  }))(
-    ifFailed,
-  )((await authenticate({ req })));
-}
 
 export const rpcMethods = { register, loginAuto, login, confirmRegistration };
